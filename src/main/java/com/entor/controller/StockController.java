0bb33ec8a8209e334313794071835f5d22f14f7f
Administrator <569869783@qq.com>
2020-01-16 16:52:04 +0800

    aaa

--- src/main/java/com/entor/controller/StockController.java
+++ src/main/java/com/entor/controller/StockController.java
@@ -1,9 +1,13 @@
 package com.entor.controller;
 
 
+import com.entor.service.IStockService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * <p>
@@ -17,5 +21,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/stock")
 public class StockController {
 
+    @Autowired
+    private IStockService stockService;
+
+    public Map<String,Object> queryByPage(Integer page,Integer limit){
+        return stockService.queryByPage(page,limit);
+    }
 }
 
